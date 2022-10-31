5097a3c8dab922e4591e5b6866c6dfd5e9623c0b
Shonkor Das <94380595+shonkor-das@users.noreply.github.com>
2022-10-31 12:58:36 +0600

    install firebase and react-router-dom

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAHvtFoFZGI25-NTppujEMaEpK8O3NY0oc",
+  authDomain: "elearning-platform-39791.firebaseapp.com",
+  projectId: "elearning-platform-39791",
+  storageBucket: "elearning-platform-39791.appspot.com",
+  messagingSenderId: "72401869564",
+  appId: "1:72401869564:web:daa5d5a4efac455d38d579"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
